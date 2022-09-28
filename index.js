52679adede4bff2f67d0952b340adc63b944d03c
DiegoGUrra <clicknamk@gmail.com>
2022-09-27 23:50:28 -0300

    added counter

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 require('dotenv').config();
+const mongoose = require("mongoose");
 const express = require('express');
 const cors = require('cors');
 const app = express();
 
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 // Basic Configuration
 const port = process.env.PORT || 3000;
+let urlSchema = new mongoose.Schema({
+  name: String,
+  short_url: {
+    type: Number,
+    unique: true
+  }
+});
+let Url = mongoose.model("Url",urlSchema);
+let counterSchema = new moongose.Schema({counter:Number});
+let Counter = moongose.model("Counter",counterSchema);
 
 app.use(cors());
 
@@ -18,7 +30,27 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
-
+const findCounter= (done)=>{
+  Counter.find({},(err,data)=>{
+    if (err){
+      done(err,null);
+    }
+    done(null,data);
+    console.log(data);
+  });
+}
+const createUrl = (url,done) => {
+  let url = new Url({name: url,short_url:1});
+  person.save((err,data)=>{
+    if (err){
+      done(err,null);
+    }
+    done(null,data);
+  });
+};
+app.get("/",(req,res,next)=>(
+  req.query.url
+));
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
