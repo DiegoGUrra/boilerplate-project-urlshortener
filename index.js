1e38d866a5ba88c67fded203c26fb302b309ff40
DiegoGUrra <clicknamk@gmail.com>
2022-09-28 00:19:32 -0300

    added express.json

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ let counterSchema = new mongoose.Schema({counter:Number});
 let Counter = mongoose.model("Counter",counterSchema);
 
 app.use(cors());
-
+app.use(express.json());
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
@@ -48,7 +48,7 @@ const createUrl = (url,done) => {
     done(null,data);
   });
 };
-app.get("/",(req,res,next)=>(
+app.post("/",(req,res,next)=>(
   req.query.url
 ));
 app.listen(port, function() {
